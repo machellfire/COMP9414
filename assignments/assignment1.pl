b35ca6a1e3ecaa0adcdf88d5ec58b69c1e868dd5
Alexander Whillas <whillas@gmail.com>
2013-03-26 17:48:29 +1100

    Assignment 1, Question 3

--- assignments/assignment1.pl
+++ assignments/assignment1.pl
@@ -2,6 +2,7 @@
 % Alexander Whillas z3446737
 %
 
+
 % Question 1
 %
 sumsq_div3or5([], 0).
@@ -19,6 +20,7 @@ sumsq_div3or5([_|Tail], Sum) :-
   sumsq_div3or5(Tail, SubTotal),
   Sum is SubTotal.
 
+
 % Question 2
 %
 givers(Giver1, Giver2) :-
@@ -27,6 +29,24 @@ givers(Giver1, Giver2) :-
   not(Giver1 == Giver2),
   not(Thing1 == Thing2).
 
-% Question 3
-%
 
+% Question 3a
+%
+log_table([], []).
+log_table([Head|Tail], ResultList) :-
+  log_table(Tail, SubList),
+  X is log(Head),
+  ResultList = [[Head, X]|SubList].
+
+% Question 3b
+%
+function_table([], _, []).
+function_table([Head|Tail], Function, ResultList) :-
+  function_table(Tail, Function, SubResult),
+  X =.. [Function, Head],
+  Y is X,
+  ResultList = [[Head|Y]|SubResult].
+
+
+% Question 4
+%
