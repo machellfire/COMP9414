70738c4a8fceae5ef7a63eb7f5f5da57a5a6556b
Alexander Whillas <whillas@gmail.com>
2013-03-26 17:14:42 +1100

    Assignment1 question 1 done

--- assignments/assignment1.pl
+++ assignments/assignment1.pl
@@ -2,4 +2,23 @@
 % Alexander Whillas z3446737
 %
 
+% Question 1
+%
+sumsq_div3or5([], 0).
+sumsq_div3or5([Head|Tail], Sum) :-
+  0 is Head mod 3,
+  !,
+  sumsq_div3or5(Tail, SubTotal),
+  Sum is Head * Head + SubTotal.
+sumsq_div3or5([Head|Tail], Sum) :-
+  0 is Head mod 5,
+  !,
+  sumsq_div3or5(Tail, SubTotal),
+  Sum is Head * Head + SubTotal.
+sumsq_div3or5([_|Tail], Sum) :-
+  sumsq_div3or5(Tail, SubTotal),
+  Sum is SubTotal.
+
+% Question 2
+%
 
